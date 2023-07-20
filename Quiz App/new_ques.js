7b8fb81355426da05a3d0658f1d939862ebc72c7
Diganta Kumar Sial <130043394+Kumardiganta@users.noreply.github.com>
2023-07-20 19:54:09 +0530

    Add files via upload

---
+++ Quiz App/new_ques.js
@@ -0,0 +1,90 @@
+let questions = [
+    {
+    numb: 1,
+    question: "MS Word is a software of?",
+    answer: "Microsoft",
+    options: [
+      "Apple",
+      "Android",
+      "Google",
+      "Microsoft"
+    ]
+},
+{
+    numb: 2,
+    question: "The valid format of MS Word?",
+    answer: ".doc",
+    options: [
+      ".exe",
+      ".doc",
+      ".png",
+      ".jpeg"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What function is used to check spelling in MS Word?",
+    answer: "Spelling & Grammer",
+    options: [
+      "Research",
+      "Word Count",
+      "Set Language",
+      "Spelling & Grammer"
+    ]
+  },
+  {
+    numb: 4,
+    question: "To show the font dialog box press?",
+    answer: "Ctrl + D",
+    options: [
+      "Ctrl + P",
+      "Ctrl + B",
+      "Ctrl + D",
+      "Ctrl + Q"
+    ]
+  },
+  {
+    numb: 5,
+    question: "A word gets selected by clicking on it ?",
+    answer: "Twice",
+    options: [
+      "Once",
+      "Twice",
+      "Three Times",
+      "Four Times"
+    ]
+  },
+  {
+    numb: 6,
+    question: "A ___ identifies a location or a selection of text that you name and identify for future reference.",
+    answer: "Bookmark",
+    options: [
+      "Footer",
+      "Clipboard",
+      "Header",
+      "Bookmark"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which option is not available in Microsoft office button?",
+    answer: "Bold",
+    options: [
+      "New",
+      "Save",
+      "Bold",
+      "Open"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In the ___ we can change the view of the document and set the zoom option.",
+    answer: "View Option Toolbar",
+    options: [
+      "Home Panel",
+      "View Option Toolbar",
+      "Ribbon",
+      "Title Bar"
+    ]
+  }
+];

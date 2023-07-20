cf101c603800708bc127751d5118b524989e13f9
Diganta Kumar Sial <130043394+Kumardiganta@users.noreply.github.com>
2023-07-20 19:49:04 +0530

    Add files via upload

---
+++ Quiz App/questions.js
@@ -0,0 +1,43 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "B. Hyper Text Markup Language",
+        option: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Markup Language",
+            "C. Hyper Text Multiple Language",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stands for?",
+        answer: "D. Cascading Style Sheet",
+        option: [
+            "A. Common Style Sheet",
+            "B. Colorful Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "A. Hypertext Preprocessor",
+        option: [
+            "A. Hypertext Preprocessor",
+            "B. Hypertext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "",
+        answer: "",
+        option: [
+
+        ]
+    },
+];
